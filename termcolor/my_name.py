d21583decf4ed513d25a7dbea6d01b6eb715abd5
bibek977 <bhattaraibibek91@gmail.com>
2023-10-18 14:16:12 +0545

    color print terminal my name

---
+++ termcolor/my_name.py
@@ -0,0 +1,20 @@
+from termcolor import colored,cprint
+
+def my_name(b):
+    print()
+    print()
+    print(f"{colored(' ______________ ',b)}")
+    print(f"{colored('|  _________   |',b)}")
+    print(f"{colored('|  |        |  |',b)}")
+    print(f"{colored('|  |        |  |',b)}")
+    print(f"{colored('|  |________|  |',b)}")
+    print(f"{colored('|   ___________|',b)}")
+    print(f"{colored('|  |___________ ',b)}")
+    print(f"{colored('|  _________   |',b)}")
+    print(f"{colored('|  |        |  |',b)}")
+    print(f"{colored('|  |        |  |',b)}")
+    print(f"{colored('|  |________|  |',b)}")
+    print(f"{colored('|______________|',b)}")
+
+
+my_name(b="green")
